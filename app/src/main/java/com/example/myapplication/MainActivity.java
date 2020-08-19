e606c82f56b3bf7a280444f6b13fe2c59c00cdb2
luoxiaohong <1765492163@qq.com>
2020-08-19 23:52:22 +0800

    添加menu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -11,6 +13,7 @@ import android.view.View.OnClickListener;
 import android.widget.Toast;
 
 
+//MainActivity实现接口OnClickListener ， MainActivity实例就是OnClickListener实例的意思
 public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
@@ -22,12 +25,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         Button button = findViewById(R.id.button);
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
-
+        Button button3 = findViewById(R.id.button3);
 
         button.setOnClickListener(this);
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
     }
+
 
 
     @Override
@@ -44,7 +49,43 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
             case R.id.button2:
                 Toast.makeText(this, "button2", Toast.LENGTH_LONG).show();
                 break;
+            case R.id.button3:
+                Toast.makeText(this, "销毁activity", Toast.LENGTH_LONG).show();
+                this.finish();
+                break;
 
         }
     }
+
+
+
+
+    /**
+     * onCreateOptionsMenu创建的菜单 ， 入口是在标题栏右上角上的三个点
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * 菜单每一项点击事件
+     * @param item
+     * @return
+     */
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+           case R.id.add_item:
+               Toast.makeText(MainActivity.this, "menu  add", Toast.LENGTH_LONG).show();
+           break;
+            case R.id.remove_item:
+                Toast.makeText(MainActivity.this, "menu  remove", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
+
 }
